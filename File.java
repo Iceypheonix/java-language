731c72e2fbd71d612f534e9fc8c58a703e0d69d2
RahulCh0udhary108 <rahulchoudharyy90@gmail.com>
2025-03-09 23:24:59 +0530

    Create File.java

---
+++ File.java
@@ -0,0 +1,14 @@
+class Student
+{
+    void name()
+    {
+        System.out.println("vasu");
+    }
+}
+public class File
+{
+    public static void main(String [] args){
+    Student st = new Student();
+    st.name();
+}
+}
